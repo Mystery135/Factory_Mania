a250a1e1d09bc320e8124f9c9672e4d217974662
Mystery135 <websiteemail12345678910>
2021-10-19 18:52:39 -0700

    Misc & added swing gui for intellij

--- src/Game_Window.java
+++ src/Game_Window.java
@@ -16,7 +16,7 @@ private static JLabel balance;
     private static JLabel payoutperiod;
     public static JLabel nextupgradedollar;
     public static JLabel nextupgradems;
-    private static ImageIcon factory = new ImageIcon("factory1.png");
+    private static ImageIcon factory = new ImageIcon("src//images//factory1.png");
 
     public static JLabel FactoryLabel;
     Game_Window(){
